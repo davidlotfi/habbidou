bf2eb24289bb7f857e56787beea44e5c837c140c
davidlotfi <boukernineabdellatif@gmail.com>
2021-08-20 17:03:59 +0200

    catex file

---
+++ catex.php
@@ -0,0 +1,67 @@
+<?php
+include('include/header.php');
+include('menu_categorie.php');
+
+$reponse=$dbd->prepare('SELECT *
+  FROM produit
+  WHERE categorie =?
+  ORDER BY id_produit DESC
+  LIMIT 0 , 16
+    ');
+$reponse->execute(array($_GET['cat']));
+?>
+
+<br><br><br><br><br><br>
+<div class="container mb-4">
+    <div class="row">
+      <div class="col-12">
+        <div class="" style="float:right">
+          <a href="catex.php"><i style='font-size:24px' class='fas'>&#xf009;</i></a>
+          <a href="list.php"><i style='font-size:24px' class='fas'>&#xf00b;</i></a>
+
+        </div>
+      </div>
+    </div><br>
+
+   <div class="row">
+
+ <?php
+   while($donnes=$reponse->fetch()){
+       $date=strtotime($donnes['date']);
+       ?>
+
+                 <div class="col-lg-3  col-md-6 mb-3">
+
+                     <div class="card" style="height:18rem;">
+                       <div class="inner">
+                         <a href="iteminfo.php?billet=<?php echo htmlspecialchars($donnes['id_produit']);?>" target="_blank" style="dicoration :none">
+                           <img class="card-img-top" src="imagesProd/<?php echo $donnes['image'];?>"style="height:150px" />
+                         </a>
+                       </div>
+
+                      <div class="card-body">
+                        <h6 class="text-success"><?php echo htmlspecialchars($donnes['prix']); ?> DA</h6>
+                      <p class="card-text"><?php echo htmlspecialchars($donnes['description']); ?></p>
+                      <small class="text-muted" style="float:right"> <?php echo date('F j',$date); ?></small>
+                      <small class="text-muted" >Habiba.com</small>
+                       <!-- <a class="card-link" href="#">About Me</a>  htanrigalha -->
+                      </div>
+
+                    </div>
+
+                  </div>
+
+
+                <?php }
+                $reponse->closeCursor();
+
+                ?>
+
+   </div><!-- row -->
+
+ </div><!-- container -->
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+<?php include('include/footer.php'); ?>
