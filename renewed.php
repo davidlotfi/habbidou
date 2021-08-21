6f4c413677fb7d4409efaa2f8e70efd26eb81f15
davidlotfi <boukernineabdellatif@gmail.com>
2021-08-21 16:25:34 +0200

    first commit

---
+++ renewed.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="images/unnamed.png">
+
+    	<script src="js/jquery.min.js"></script>
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="web-fonts-with-css/css/fontawesome-all.min.css">
+
+      <title>Vender vos Produit ...</title>
+   <style media="screen">
+        .imheigt{
+          height: 150px;
+          width: 270px;
+          margin-right: 25px;
+          position: relative;
+        }
+        .opcity:hover{
+          opacity: 0.7;
+          filter: alpha(opacity=50); /* For IE8 and earlier */
+        }
+
+  </style>
+</head>
+<body style="">
+
+ <header>
+   <!-- Navigation -->
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+     <div class="container">
+       <a class="navbar-brand" href="index.php">Habiba.com</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarResponsive">
+         <ul class="navbar-nav ml-auto">
+           <li class="nav-item">
+            <!-- <a class="nav-link" href="index.php">Home</a>-->
+           </li>
+           <li class="nav-item">
+            <!--   <a class="nav-link" href="#">About</a> -->
+           </li>
+           <li class="nav-item">
+              <!-- <a class="nav-link" href="#">Services</a> -->
+           </li>
+           <li class="nav-item">
+            <!--   <a class="nav-link" href="#">Contact</a> -->
+           </li>
+         </ul>
+       </div>
+    </div>
+   </nav>
+
+ </header>
+<br><br><br><br>
+<div class="container mb-4">
+  <h3 style="color : var(--pink);">Commencez à vender vos produits et développez votre activité</h3><br>
+  <h5 class="text-center text-primary">je vender quoi ?</h5>
+  <div class="row text-center">
+      <section class="col-lg-2">
+        <img src="images/games.png" alt="">
+        <p>Consoles et jeux vidéo</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/laptop.png" alt="">
+        <p>Ordinateurs portables </p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/mobile.png" alt="">
+        <p>Smartphones</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/tablette.png" alt="">
+        <p>Tablettes</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/camera.png" alt="">
+        <p>Appareils photo</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/audio.png" alt="">
+        <p>Casques & audio</p>
+      </section>
+  </div><!-- row -->
+
+  <div class="row text-center">
+      <section class="col-lg-2">
+        <img src="images/tv.png" alt="">
+        <p>Télévisions</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/bureau.png" alt="">
+        <p>Matériel de bureau </p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/tools.png" alt="">
+        <p>Outils domestiques</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/watch.png" alt="">
+        <p>Montres d'occasion</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/elctromenage.png" alt="">
+        <p>Appareils électroménagers</p>
+      </section>
+      <section class="col-lg-2">
+        <img src="images/kitchen.png" alt="">
+        <p>Petit électroménager</p>
+      </section>
+  </div><!-- row2 -->
+
+
+</div>
+<div class="container">
+    <h3 class="text-success">Comment puis-je commencer à vendre ?</h3>
+        <h5 class="text-primary"> <img src="images/check.25.png" alt="" style="">  Enregistrez-vous pour devenir vendeur sur Habiba.com </h5>
+        <p>Si vous n'avez encore jamais vendu des produits sur Habiba.com, il vous suffit de vous inscrire en utilisant votre adresse e-mail. <a href="inscrir.php" target="_blank">S'inscrire ici</a></p>
+
+</div>
+
+<div class="container mb-3">
+   <div class="row">
+     <section class="col-lg-5">
+         <img class="img-thumbnail opcity" src="imagecat\dung-tr-n-vi-t-4ZtrbKfJQHs-unsplash.jpg" alt="">
+     </section>
+      <section class="col-lg-7">
+        <div class="row no-gutters">
+          <div class="">
+              <img class="img-thumbnail imheigt opcity" src="imagecat\hello-i-m-nik-m7PaAQkxgxw-unsplash.jpg" alt="">
+          </div>
+          <div class="">
+              <img class="img-thumbnail imheigt opcity" src="imagecat\william-iven-TMOeGZw9NY4-unsplash.jpg" alt="">
+          </div>
+          <div class="">
+              <img class="img-thumbnail imheigt opcity" src="imagecat\frankjsksj.jpg" alt="">
+          </div>
+          <div class="">
+              <img class="img-thumbnail imheigt opcity" src="imagecat\qx2J3TsRsk.jpg" alt="">
+          </div>
+        </div>
+
+
+      </section>
+
+   </div>
+</div>
+
+<?php include('include/footer.php'); ?>
