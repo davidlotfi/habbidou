32852e6e3f0c321eeaf4e1fa623c0ae68d7c0f70
davidlotfi <boukernineabdellatif@gmail.com>
2021-08-21 16:24:39 +0200

    first commit

---
+++ menu_categorie.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="web-fonts-with-css/css/fontawesome-all.min.css">
+<link href="css/menu-prencipel.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="catex.php?cat=8"<i style="font-size:18px" class="fa fa-camera-retro" style="font-size:24px"></i> Telephone&Tablettes</a>
+  <a href="catex.php?cat=3"><i class="fa fa-bath"></i> Maison & jardin</a>
+  <a href="catex.php?cat=10"><i class="far fa-grin-alt">&#xf581;</i> Bébé & Kids</a>
+  <a href="catex.php?cat=11"><i style="font-size:24px" class="fas fa-birthday-cake"></i> food</a>
+  <a href="catex.php?cat=12"><i class='fas fa-car'></i> Voitures & Motos</a>
+  <a href="catex.php?cat=9"><i class="fa fa-tv" style="font-size:16px"></i> Electromenager</a>
+  <a href="catex.php?cat=1"><i class="fa fa-laptop"></i> Informatique</a>
+  <a href="catex.php?cat=4"><i class="fa fa-gamepad"></i> Jeux & Consoles</a>
+  <a href="catex.php?cat=2"><i class="fa fa-graduation-cap"></i> Liver</a>
+  <a href="catex.php?cat=5"><i class="fa fa-child"></i> Mode Homme</a>
+  <a href="catex.php?cat=6"><i class="fa fa-shopping-bag"></i> Mode Femme</a>
+  <a href="catex.php?cat=7"><i style="font-size:20px" class="fa">&#xf1e3;</i> Sports</a>
+  <a href="list.php">Autres Categoriers</a>
+</div>
+
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "240px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+
+</body>
+</html>
