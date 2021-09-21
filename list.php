d89fe275e2fdced52113d34c9d447e7711229b03
davidlotfi <boukernineabdellatif@gmail.com>
2021-09-21 23:13:46 +0200

    fix

--- list.php
+++ list.php
@@ -1,89 +1,91 @@
 <?php
-  //include('include/Connexion.php');
+  require_once("include/pagination.php");
   include('include/header.php');
   include('menu_categorie.php');
-  $reponse=$dbd->query('select * from produit,brands WHERE produit.marque = brands.id ORDER BY id_produit DESC');
+  //$reponse=$dbd->query('select * from produit,brands WHERE produit.marque = brands.id ORDER BY id_produit DESC');
  ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title></title>
-  </head>
-  <style media="screen">
-   body{
-    background-color: #F5F5F5;
-   }
-  .container img{
-      width:200px;
-      border-radius: 12px;
-      margin-right: 15px;
+ <?php
 
-    }
-    .ullist{
+        // Pagination
+        //1. the current page number ($current_page)
+          $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+        //2. Produit per page ($per_page)
+          $per_page = 12;
+        //3. total count Produit ($total_count)
+          $total_count = count_all_produit_for_list();
+        //4. Object of pagination
+          $pagination = new Pagination($page,$per_page,$total_count);
 
-      background: #FDF6E3;
-      width: 100%;
-      height:180px;
-      padding-bottom:25px;
-      padding-left:15px;
-      //border-radius: 0 0 20px 20px;
-      border-bottom: 1px dotted black;
-      margin-bottom: 0px;
+          $per_page=addslashes($per_page);
+          $offset=$pagination->offset();
+          $offset=addslashes($offset);
 
-    }
-    .lilist{
-      list-style: none;
-      text-decoration:inherit;
-      margin:0;
-      padding: 10px 10px 0 10px;
-    }
-    .btlist{
-      float:right;
-      margin:auto;
-    }
-    .list-group{
-      margin-top: 10px;
-      position: fixed;
-    }
-    .list-group-item{
-      padding:8px;
-    }
-  </style>
-  <body>
+          $sql  = "SELECT * FROM produit";
+          $sql .= " ORDER BY id_produit DESC";
+          $sql .= " LIMIT {$per_page}";
+          $sql .= " OFFSET {$offset}";
+
+          $reponse = find_by_sql($sql);
+
+  ?>
+
     <br><br><br><br>
      <div class="container col-lg-8 mb-4">
-
        <?php
          while($donnes=$reponse->fetch()){
-         $content = $donnes['imagereal'];
-         $datelist= strtotime($donnes['date']);
+
+           $datelist= strtotime($donnes['date']);
              ?>
        <ul class="ullist">
          <li class="lilist">
-
-           <img class="float-left" src="imagesProd/<?php if(empty($donnes['image'])){
+           <img class="float-left imaglist" src="imagesProd/<?php if(empty($donnes['image'])){
                                        echo'image.png';}echo htmlspecialchars($donnes['image']);?>"style="height:150px" />
            <div class="info">
-             <h6 class="card-title text-primary"><?php echo htmlspecialchars($donnes['nom']); ?></h6>
-
+             <h6 class="card-title text-primary"><?php echo htmlspecialchars('habbidou.com'); ?></h6>
            </div>
            <figcaption><?php echo htmlspecialchars($donnes['description']); ?></figcaption>
            <h6 class="text-success"><?php echo htmlspecialchars($donnes['prix']); ?>  DA</h6>
            <small class="text-muted"><?php echo htmlspecialchars(date('Y-m-d à H:i',$datelist)); ?> </small>
             <a href="iteminfo.php?billet=<?php echo htmlspecialchars($donnes['id_produit']);?>" class="btn btn-primary btlist" target="_blank">Détails</a>
          </li>
-
        </ul>
-     <?php }
-     $reponse->closeCursor();
-
-      ?>
-
+         <?php }
+             $reponse->closeCursor();
+          ?>
      </div>
 
-    
+     <nav aria-label="...">
+             <ul class="pagination justify-content-center">
+                <?php if($pagination->has_previous_page()){?>
+                 <li class="page-item">
+                     <a class="page-link" href="<?php echo 'list.php?page='.$pagination->previous_page(); ?>" tabindex="-1">Previous</a>
+                 </li>
+               <?php }else{ ?>
+                 <li class="page-item disabled">
+                     <a class="page-link" href="#" tabindex="-1">Previous</a>
+                 </li>
+               <?php } ?>
+
+                   <?php for($i=1; $i <= $pagination->total_pages(); $i++){ ?>
+                     <?php if($i == $page){?>
+                      <li class="page-item active">
+                          <a class="page-link" href="#"><?php echo $i; ?><span class="sr-only">(current)</span></a>
+                       </li>
+                     <?php }else{ ?>
+                         <li class="page-item"><a class="page-link" href="<?php echo 'list.php?page='.$i; ?>"><?php echo $i; ?></a></li>
+                     <?php } ?>
+                    <?php }?>
+
+               <?php if($pagination->has_next_page()){ ?>
+                   <li class="page-item">
+                        <a class="page-link" href="<?php echo 'list.php?page='.$pagination->next_page(); ?>">Next</a>
+                   </li>
+               <?php }else{ ?>
+                   <li class="page-item disabled">
+                        <a class="page-link" href="#">Next</a>
+                   </li>
+             <?php } ?>
+             </ul>
+        </nav>
+
   <?php include('include/footer.php'); ?>
